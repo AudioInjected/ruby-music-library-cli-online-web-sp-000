414b4d74b3fd6c8ac0825ca217d90337eeee64ef
Rafael Batista <rafaelb24@yahoo.com>
2018-11-16 01:02:34 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,7 +15,6 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets until input == "exit"
-    binding.pry
   end
 
   def list_songs
